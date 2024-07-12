408f1892939f0c6c45bc7288f7b7bbbcd529b641
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-07-12 09:16:17 +0530

    backend api and database added, added admin daashboard, added upload blog features

---
+++ app/admin/blogList/page.jsx
@@ -0,0 +1,11 @@
+"use Client";
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>BlogList page</div>
+  )
+}
+
+export default page
---
+++ app/admin/subscriptions/page.jsx
@@ -0,0 +1,12 @@
+"use Client";
+
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Subscriptions page</div>
+  )
+}
+
+export default page
