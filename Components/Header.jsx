878dacc9a1bea0872c1169d602aae2c1de9e39e0
Bodhisatwa Chakraborty <bodhisatwachakraborty2002@gmail.com>
2024-07-13 20:34:59 +0530

    added email subscription functionality

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,8 +1,25 @@
 import { assets } from "@/Assets/assets";
+import axios from "axios";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const Header = () => {
+  const [email, setEmail] = useState("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("email", email);
+    const response = await axios.post("/api/email", formData);
+    if (response.data.success) {
+      toast.success(response.data.msg);
+      setEmail("");
+    } else {
+      toast.error("Error");
+    }
+  };
+
   return (
     <div className="py-5 px-5 md:px-12 lg:px-28">
       <div className="flex justify-between items-center">
@@ -17,16 +34,20 @@ const Header = () => {
         </button>
       </div>
       <div className="text-center my-8">
-        <h1 className="text-3xl sm:text-5xl font font-medium">Latest Blogs</h1>
-        <p className="mt-10 mx-w-[740px] m-auto text-xs sm:text-base">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, ea sapiente vitae nihil quia at nisi quibusdam, animi beatae corrupti, eligendi tempora! Facilis quis adipisci, blanditiis consequatur at officiis eos fugiat velit delectus, recusandae et magni pariatur labore aut architecto mollitia provident eveniet repellat dignissimos numquam incidunt? Nisi, consequatur perspiciatis.
+        <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
+        <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text
+          ever.
         </p>
         <form
+          onSubmit={onSubmitHandler}
           className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]"
           action=""
         >
           <input
-            
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             type="email"
             placeholder="Enter your email"
             className="pl-4 outline-none"
